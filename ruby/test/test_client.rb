718952f0147adeffd82d3bd59ba202650d768f00
KATO Kazuyoshi <kzys@8-p.info>
2009-12-16 23:45:44 +0900

    Added a test of "request".

--- ruby/test/test_client.rb
+++ ruby/test/test_client.rb
@@ -2,13 +2,42 @@ require 'test/unit'
 require 'google/chrome/client'
 require 'stringio'
 
+class FakeIO
+  def initialize(str = '')
+    @io = StringIO.new(str, 'r')
+  end
+
+  def write(s)
+    ;
+  end
+
+  def gets
+    @io.gets
+  end
+
+  def read(size)
+    @io.read(size)
+  end
+end
+
 class ClientTest < Test::Unit::TestCase
   def test_new
     assert_raise Google::Chrome::HandshakeError do
-      Google::Chrome::Client.new(StringIO.new)
+      Google::Chrome::Client.new(FakeIO.new)
     end
 
-    io = StringIO.new("ChromeDevToolsHandshake\r\n" * 2, 'r+')
+    io = FakeIO.new("ChromeDevToolsHandshake\r\n")
     assert_kind_of(Google::Chrome::Client.new(io), Google::Chrome::Client)
+  end
+
+  def test_request
+    io = FakeIO.new([ "ChromeDevToolsHandshake\r\n",
+                      "Content-Length:2\r\n",
+                      "\r\n",
+                      "{}" ].join(''))
+    client = Google::Chrome::Client.new(io)
+    header, resp = client.request({}, {})
+    assert_equal(header, { 'Content-Length' => '2' })
+    assert_equal(resp, {})
   end
 end
