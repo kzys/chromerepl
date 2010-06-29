cfc24651e2a3b9ae26c2084af3e6e34becd23b66
KATO Kazuyoshi <kato.kazuyoshi@gmail.com>
2010-06-29 23:37:26 +0900

    Added test of eval_string.

--- ruby/test/test_repl.rb
+++ ruby/test/test_repl.rb
@@ -1,15 +1,34 @@
 require 'test/unit'
+require 'minitest/mock'
 require 'google/chrome/repl'
 
-class Extension
+class Extension < MiniTest::Mock
+  def connect(&block)
+    block.call(0)
+  end
 end
 
 class Client
   def initialize(port)
   end
 
+  def read_all_response
+    []
+  end
+
+  @@extension = nil
+
   def extension(id)
-    Extension.new
+    Client.extension
+  end
+
+  def self.extension=(e)
+    @@extension = e
+  end
+
+  def self.extension
+    @@extension ||= Extension.new
+    @@extension
   end
 end
 
@@ -23,5 +42,15 @@ class REPLTest < Test::Unit::TestCase
   def test_new
     app = TestableREPL.new(nil, nil)
     assert_kind_of(app, Google::Chrome::REPL)
+  end
+
+  def test_eval_string
+    s = '(function () { 1+arguments[0]; }).apply(null, [2])'
+    Client.extension.expect('post', nil, [0, s])
+
+    app = TestableREPL.new(nil, nil)
+    app.eval_string('1+arguments[0]', [2])
+
+    Client.extension.verify
   end
 end
