4b5c459dcab8a31967cb1e20379a516cc1ff9eb6
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2010-10-10 11:57:13 +0900

    Closed #1. http://utatane-constellation.tumblr.com/post/851555092

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,7 @@
 var originalLog = console.log;
 
+var globalEval = eval;
+
 chrome.extension.onConnect.addListener(function (port) {
     port.onMessage.addListener(function (message, info) {
         console.log = function (s) {
@@ -8,7 +10,7 @@ chrome.extension.onConnect.addListener(function (port) {
         };
 
         try {
-            var obj = eval(message);
+            var obj = globalEval(message);
             port.postMessage({ success: obj });
         } catch (e) {
             port.postMessage({ error: e });
